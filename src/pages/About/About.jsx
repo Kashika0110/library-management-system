3029083fe69eb2710d547f806add5fc5ab67ee7f
Kashika0110 <137630129+Kashika0110@users.noreply.github.com>
2023-06-24 22:42:04 +0530

    Add files via upload

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./About.css";
+const About = () => {
+  return (
+    <section className='about'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>About</h2>
+        </div>
+
+        <div className='about-content grid'>
+        <div className='about-text'>
+            <p className='fs-17'>Hi I am Kashika pursuing Computer Science degree in Kumaraguru College Of Technology. </p>
+            <p className='fs-17'>I have designed this website using React JS and jsx as templating language.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
